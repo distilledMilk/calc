c4b87198f494b010520aa7c661f907df3ff2d005
distilledMilk <atharva.velani@gmail.com>
2025-06-04 14:25:43 +0800

    default functions ready

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function add(a, b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+
+console.log(subtract(1,3));
+console.log(add(1,3));
+console.log(multiply(1,3));
+console.log(divide(1,3));
