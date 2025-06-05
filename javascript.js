7f579a43c6ed65bd347ae6d3e78096d9cf91689b
distilledMilk <atharva.velani@gmail.com>
2025-06-05 12:29:19 +0800

    completed

--- javascript.js
+++ javascript.js
@@ -14,24 +14,83 @@ function divide(a,b){
     return a/b;
 }
 
-function operate(operator, a, b){
-    switch(operator){
-        case '-':
-            subtract(a, b);
-            break;
-        case '+':
-            add(a,b);
-            break;
-        case '/':
-            divide(a,b);
-            break;
-        default:
-            multiply(a,b);
-    }
+function mod(a,b){
+    return a%b;
 }
 
+function operate(){
+    if(a === '' || b === '') return;
+    console.log(`A at start of operation: ${a} B at start of operation ${b} operator start of operation: ${operator}`);
+    let aO = parseFloat(a);
+    let bO = parseFloat(b);
+    let value = '';
+    switch(operator){
+        case '-':
+             value = subtract(bO, aO);
+             break;
+        case '+':
+            value = add(bO, aO);
+            break;
+        case '/':
+            value = divide(bO, aO);
+            break;
+        case 'x':
+            value = multiply(bO, aO);
+            break;
+        default:
+            value = mod(bO, aO);
+            break;
+    }
+    a = value;
+    operator = '';
+    b = '';
+    console.log('did calculations: ' + value);
+    
+    document.querySelector('#screen').value = `${value}`
 
-console.log(subtract(1,3));
-console.log(add(1,3));
-console.log(multiply(1,3));
-console.log(divide(1,3));
+
+}
+
+let a = ''
+let operator = '';
+let b = '';
+let length = document.querySelector('#screen').value.length;
+function appendNumber(num){
+    if(a.length > 10) return;
+    console.log("Length: " + length);
+    a += num;
+    document.querySelector('#screen').value = `${b} ${operator} ${a}`;
+}
+
+function deleteNumber(){ 
+    a = a.slice(0, -1);
+    console.log("text after pop: " + a);
+    document.querySelector('#screen').value = a;
+}
+function clearScreen(){
+    a = '';
+    operator = '';
+    b = '';
+    document.querySelector('#screen').value = '';
+}
+
+function appendOperator(aOperator){
+    if(a === ''){ console.log('met here'); return};
+
+    if(b != ''){
+        console.log('b is not empty');
+        console.log('b:' + b);
+        operate();
+        // document.querySelector('#screen').value = `${a}`
+        // return;
+    }
+    console.log('a: ' + a);
+
+    operator = aOperator; 
+    console.log('operator: ' + operator);
+    b = a;
+    console.log('b: ' + b);
+    a = '';
+    console.log('a: ' + a);
+    document.querySelector('#screen').value = `${b} ${operator}`;
+}
